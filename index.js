07d7f8ffbbf4b15b79fa1e2eef5514d5a23f2890
Luciano Gallo <102675296+luchogallo92@users.noreply.github.com>
2024-03-28 17:04:51 -0300

    1.1

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const pizzas = [
   {
     id: 1,
-    nombre: "pizza de Muzzarella",
+    nombre: "Pizza de Muzzarella",
     precio: 500,
     ingredientes: ["Muzzarella", "Tomate", "Aceitunas"],
     imagen: "./img/muzzarella.png",
@@ -9,7 +9,7 @@ const pizzas = [
 
   {
     id: 2,
-    nombre: "pizza de Cebolla",
+    nombre: "Pizza de Cebolla",
     precio: 1500,
     ingredientes: ["Muzzarella", "Tomate", "Cebolla"],
     imagen: "./img/cebolla.png",
@@ -17,7 +17,7 @@ const pizzas = [
 
   {
     id: 3,
-    nombre: "pizza 4 Quesos",
+    nombre: "Pizza 4 Quesos",
     precio: 1380,
     ingredientes: [
       "Muzzarella",
@@ -31,7 +31,7 @@ const pizzas = [
 
   {
     id: 4,
-    nombre: "pizza Especial",
+    nombre: "Pizza Especial",
     precio: 1000,
     ingredientes: ["Muzzarella", "Tomate", "Rucula", "Jamón"],
     imagen: "./img/especial.png",
@@ -39,7 +39,7 @@ const pizzas = [
 
   {
     id: 5,
-    nombre: "pizza con Anana",
+    nombre: "Pizza con Anana",
     precio: 600,
     ingredientes: ["Muzzarella", "Tomate", "Anana"],
     imagen: "./img/anana.png",
@@ -77,15 +77,17 @@ const inputuser = searchbox.addEventListener ('submit', box_user_search)
 
 
 
-// //Renderizado de las cards
-// const rendercard (pizza) = box_user_search.find {
-//   const {id, nombre, precio, ingredientes, imagen}
-//   return `<div class="pizza-card">
-//   <img src="${pizza.imagen}" alt="">
-//   <div class="pizza-info">
-//       <h4>${pizza.nombre}</h4>
-//       <span>${pizza.ingredientes}</span>
-//       <span>Precio $${pizza.precio}</span>
-//   </div>
-// </div>`
-// }
+//Renderizado de las cards
+const rendercard = (pizza) => {
+  const {id, nombre, precio, ingredientes, imagen} = pizza
+  return `
+  <img src="${imagen}" alt="">
+  <div class="pizza-info">
+      <h4>${nombre}</h4>
+      <span>${ingredientes}</span>
+      <span>Precio $${precio}</span>
+  </div>`
+}
+
+renderpizza.innerHTML = pizzas.map(rendercard).join('')
+
